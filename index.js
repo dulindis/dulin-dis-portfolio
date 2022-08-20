ed0fdbf538ebaa6bcbd7bb324c2040f6c34ffa72
POkulska <paulina.okulska@gmail.com>
2022-08-20 17:41:55 +0200

    changing resolution path in server

--- index.js
+++ index.js
@@ -54,10 +54,10 @@ if (process.env.NODE_ENV === "production") {
   // Serve any static files
   console.log('we are on production!')
 
-  app.use(express.static(path.resolve(__dirname, "../client/build")));
+  app.use(express.static(path.resolve(__dirname, "/client/build")));
    // Handle React routing, return all requests to React app  
    app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client","build", "index.html"));
   });
 } 
 
