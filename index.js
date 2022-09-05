d46c17669e9e449fedba4f716e05035e521f7e75
POkulska <paulina.okulska@gmail.com>
2022-09-05 20:46:00 +0200

    removed slash from client

--- index.js
+++ index.js
@@ -51,10 +51,10 @@ app.use('/api',contactRoutes.routes);
 app.use(express.static(path.join(__dirname, "/client/public")));
 
 if (process.env.NODE_ENV === `production` || process.env.NODE_ENV === `staging`) {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-  // app.get(`*`, (req, res) => {
-  // res.sendFile(path.join(__dirname + `/client/build/index.html`));
-  // });
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get(`*`, (req, res) => {
+  res.sendFile(path.join(__dirname + `client/build/index.html`));
+  });
  }
 
 app.listen(PORT,HOST, ()=>console.log(`App is listening on host ${HOST} and port: ${PORT}`));
